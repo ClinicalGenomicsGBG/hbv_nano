5473439d31898299125d4aa2a75c63e4f7ea51f2
Daniel Schmidt <daniel.e.schmidt@outlook.com>
2023-08-22 11:58:21 +0200

    general update

--- consensus.sh
+++ consensus.sh
@@ -25,17 +25,17 @@ ref_h=/Users/xschmd/Desktop/referensgenom/ref_h.fa
 ref_i=/Users/xschmd/Desktop/referensgenom/ref_i.fa
 
 #sorted=/Users/daniel/Desktop/hbv/validering/hbv_val_05/barcode06/bc06.fa_al_d_sorted.bam     #sorted bam file with reads
-sorted=/Users/xschmd/Desktop/validering/hbv_val_06/fastq_pass/barcode11/bc11_filtered.fa_al_d_sorted.bam
+sorted=/Users/xschmd/Desktop/validering/hbv_val_06/fastq_pass/barcode1/bc11_filtered.fa_al_d_sorted.bam
 out="${sorted:0:-11}_consensus_d20.fasta"
 
 #samtools mpileup -uf  $ref_d $sorted | bcftools call -c --ploidy 1 | vcfutils.pl vcf2fq -d1000000000 > $out     #change ref_x   #can add -d20 for vcfutils to only include reads with depth >20
                                                                                                         #standard is -d3
-bcftools mpileup -f $ref_d $sorted | bcftools call -c --ploidy 1 | vcfutils.pl vcf2fq -d20 > $out     #change ref_x   #can add -d20 for vcfutils to only include reads with depth >20
+#bcftools mpileup -f $ref_d $sorted | bcftools call -c --ploidy 1 | vcfutils.pl vcf2fq -d20 > $out     #change ref_x   #can add -d20 for vcfutils to only include reads with depth >20
 
 
-#bcftools mpileup -Ou -f $ref_d $sorted | bcftools call -mv -Oz -o /Users/xschmd/Desktop/test.vcf.gz
-#bcftools index /Users/xschmd/Desktop/test.vcf.gz
-#bcftools consensus -f $ref_d /Users/xschmd/Desktop/test.vcf.gz > /Users/xschmd/Desktop/test.fa
+bcftools mpileup -Ou -f $ref_d $sorted | bcftools call -mv -Oz -o /Users/xschmd/Desktop/test2.vcf.gz
+bcftools index /Users/xschmd/Desktop/test2.vcf.gz
+bcftools consensus -f $ref_d /Users/xschmd/Desktop/test2.vcf.gz > /Users/xschmd/Desktop/test2.fa
 
 echo $out
 
