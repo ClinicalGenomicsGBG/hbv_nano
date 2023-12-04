51f08809980f78c2a89a5c83b1db6ec66ea5c89b
Daniel Schmidt <daniel.schmidt@gu.se>
2023-12-04 17:17:59 +0100

    Select bam files using csv file.

--- workflow/scripts/error_rate.py
+++ workflow/scripts/error_rate.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
 import pandas as pd
-#import snakemake
 
 input_file = "samtools/error_rates.csv"
 output_file = "samtools/minimum_error_rates.csv"
+output_file_2 = "samtools/minimum_error_rates_2.csv"
 
 df = pd.read_csv(input_file, header=None)    # import error rates
 df.columns = ["read_id", "ref", "error_rate"]
 df = df.loc[df.groupby("read_id")["error_rate"].idxmin()].reset_index(drop=True)   # get the sample with the lowest error rate for each read_id
+
 df.to_csv(output_file, index=False)   # export to csv
+
+new_df = df["read_id"] + "." + df["ref"] + ".bam"   # df with all the relevant bam files.
+new_df.to_csv(output_file_2, index=False, header=False)
