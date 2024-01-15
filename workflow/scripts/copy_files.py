8b548aac5c846067d7b692fee9af05616df6c923
Daniel Schmidt <daniel.schmidt@gu.se>
2024-01-15 16:00:30 +0100

    File renaming. Added rule for medaka variant.

--- workflow/scripts/copy_files.py
+++ workflow/scripts/copy_files.py
@@ -9,15 +9,17 @@ def copy_files():
     for _, row in df.iterrows():
         read_id = row['read_id']
         ref = row['ref']
-        bam_file = f'samtools/{read_id}.{ref}.bam'
+        #bam_file = f'samtools/{read_id}.{ref}.bam'
+        consensus_file = f'consensus/{read_id}.{ref}.fa'
         medaka_dir = f'consensus/medaka/{read_id}.{ref}'
         output_dir = f'output/medaka/{read_id}.{ref}'
         if not os.path.exists('output'):
             os.makedirs('output')
-        shutil.copy(bam_file, 'output')
+        #shutil.copy(bam_file, 'output')
+        shutil.copy(consensus_file, 'output')
         shutil.copytree(medaka_dir, output_dir)
 
     with open('output/copy_files_done.txt', 'w') as f:
-        f.write('copy_bams done')
+        f.write('file copying done')
 
 copy_files()                                                                       
