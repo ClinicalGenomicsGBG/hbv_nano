f90ed25d8c2dc23afc49b796b2e6c5e67114a052
Daniel Schmidt <daniel.schmidt@gu.se>
2024-02-13 13:47:12 +0100

    Generalised variant calling.

--- workflow/scripts/vcf_pandas.py
+++ workflow/scripts/vcf_pandas.py
@@ -1,47 +1,53 @@
 #!/usr/bin/env python3
-
+#GENERALISING!!!
 import pandas as pd
 
-# Read the header lines
-with open('freebayes/bc17.ref_d.vcf', 'r') as f:
-    lines = f.readlines()
-    header_line = next(line for line in lines if line.startswith('#CHROM'))
-    header = header_line.strip().split('\t')
+df_files = pd.read_csv('samtools/minimum_error_rates.csv')
 
-# Read the data lines into a DataFrame
-df = pd.read_csv('freebayes/bc17.ref_d.vcf', comment='#', sep='\t', names=header)
+for _, row in df_files.iterrows():
+    read_id = row['read_id']
+    ref = row['ref']
 
-# Split the 'unknown' column into multiple columns
-split_df = df['unknown'].str.split(':', expand=True)
-split_df.columns = ['GT', 'DP', 'AD', 'RO', 'QR', 'AO', 'QA', 'GL']
+    vcf_in_path = f'freebayes/{read_id}.{ref}.vcf'
+    vcf_out_path = f'freebayes/{read_id}.{ref}_edit.vcf'
+    # Import vcf to df
+    with open(vcf_in_path, 'r') as f:
+        vcf_in = f.readlines()
+        header_line = next(line for line in vcf_in if line.startswith('#CHROM'))
+        header = header_line.strip().split('\t')
 
-# Concatenate the original DataFrame with the new DataFrame
-df = pd.concat([df, split_df], axis=1)
+        f.seek(0)
+        
+        df = pd.read_csv(f, comment='#', sep='\t', names=header)
 
-df['GT'] = ' ' + df['GT']
+    if 'unknown' in df.columns and not df['unknown'].empty:
+        split_df = df['unknown'].str.split(':', expand=True)                   # Split the 'unknown' column
+        split_df.columns = ['GT', 'DP', 'AD', 'RO', 'QR', 'AO', 'QA', 'GL']    # and name the columns      
+    else:
+        print(f'Column "unknown" does not exist/is empty in {vcf_in_path}! Skipping...')
+        continue
 
-#print(df)
-# Split the 'AO' column into multiple columns
-split_ad = df['AO'].str.split(',', expand=True)
+    df = pd.concat([df, split_df], axis=1)    # Concatenate the df with the split columns
 
-# Convert 'RO' column to numeric
-df['RO'] = pd.to_numeric(df['RO'])
+    df['GT'] = ' ' + df['GT']    # Add space to avoid date conversion if vcf opened in Excel
 
-# Convert 'AO' column to list of numeric values and calculate AO/(AO+RO)
-for i in range(len(split_ad.columns)):
-    ao_values = pd.to_numeric(split_ad[i])
-    print(ao_values)
-    df[f'freq_{i+1:02}'] = (ao_values / (ao_values + df['RO'])).round(3)
+    # Split the 'AO' column into multiple columns
+    split_ao = df['AO'].str.split(',', expand=True)
 
-metadata = ''.join([line for line in lines if line.startswith('##')])
-df_str = df.to_csv(sep='\t', index=False, header=True)
-output = metadata + df_str
+    df['RO'] = pd.to_numeric(df['RO'])
 
-# Print the 'freq' columns
-print(df.filter(like='freq'))
+    # Convert 'AO' column to list of numeric values and calculate AO/(AO+RO)
+    for i in range(len(split_ao.columns)):
+        ao_values = pd.to_numeric(split_ao[i])
+        df[f'freq_{i+1:02}'] = (ao_values / (ao_values + df['RO'])).round(3)
 
-# Write the DataFrame to the file
-with open('freebayes/bc17.ref_d_edit_pandassssss.vcf', 'w') as f:
-    f.write(output)
+    vcf_metadata = ''.join([line for line in vcf_in if line.startswith('##')])
+    vcf_data = df.to_csv(sep='\t', index=False, header=False)
+    output = vcf_metadata + vcf_data
 
-#print(df)
+    #print(vcf_data)
+    # Write the DataFrame to the file
+    with open(vcf_out_path, 'w') as f:
+        f.write(output)
+
+#print(df.filter(like='freq').iloc[1:11])
