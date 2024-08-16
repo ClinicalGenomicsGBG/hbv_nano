93171a94a9b3163ad4deaab368f617f4a390e7cf
Jens P Werling <jens.p.werling@gmail.com>
2024-08-16 15:39:02 +0200

    Update incorrect medaka dockerhub url

--- install/build_images.sh
+++ install/build_images.sh
@@ -11,6 +11,6 @@ mkdir -p "${root_image_path}"
 # Run image build commands
 apptainer build "${root_image_path}/fastp_0.23.4.img" docker://staphb/fastp:0.23.4
 apptainer build "${root_image_path}/freebayes_1.3.7.img" docker://staphb/freebayes:1.3.7
-apptainer build "${root_image_path}/medaka_1.12.0.img" docker://ontresearch/medaka:1.12.0
+apptainer build "${root_image_path}/medaka_1.12.0.img" docker://ontresearch/medaka:sha3486abaab0d3b90351617eb8622acf2028edb154
 apptainer build "${root_image_path}/minimap2_2.28.img" docker://staphb/minimap2:2.28
 apptainer build "${root_image_path}/samtools_1.20.img" docker://staphb/samtools:1.20
