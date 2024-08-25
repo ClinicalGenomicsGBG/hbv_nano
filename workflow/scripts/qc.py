a9b45aacba5e88b9eb49a93bd9f3182d825894f8
Daniel Schmidt <daniel.schmidt@gu.se>
2024-08-25 19:05:40 +0200

    Cleared up qc.py by move to different functions.

--- workflow/scripts/qc.py
+++ workflow/scripts/qc.py
@@ -4,62 +4,88 @@ import pandas as pd
 import yaml
 import os
 
-# Get output folder using snakemake or, if running script independently, directly from config file
-try:
-    output = snakemake.params.output
-except NameError:
-    with open('config/config.yaml', 'r') as f:
-        config = yaml.safe_load(f)
-    output = config['output']
 
-# Get the best matching samples
-df_error_rates = pd.read_csv(f'{output}/samtools/minimum_error_rates.csv')
-df_error_rates = df_error_rates[['read_id','ref']]
+def get_output():
+    '''Get output folder using snakemake or, if running script independently, directly from config file'''
+    
+    try:
+        output = snakemake.params.output
+    except NameError:
+        with open('config/config.yaml', 'r') as f:
+            config = yaml.safe_load(f)
+        output = config['output']
+    return output
 
-# Get the number of mapped reads for each sample
-df_view_stats = pd.read_csv(f'{output}/samtools/view_stats.csv', header=None)
-df_view_stats.columns = ['read_id', 'ref', 'mapped_reads']
+def data_handling():
+    '''Get mapped reads best matching samples and negative controls and calculate ratios'''
 
-# Get the number of mapped reads in the rt region for each sample
-df_view_stats_rt = pd.read_csv(f'{output}/samtools/view_stats_rt.csv', header=None)
-df_view_stats_rt.columns = ['read_id', 'ref', 'mapped_reads_rt']
+    output = get_output()
 
-# Get the number of mapped reads for the best matching samples
-df_merged = pd.merge(df_error_rates, df_view_stats, on=['read_id', 'ref'])
-df_merged = pd.merge(df_merged, df_view_stats_rt, on=['read_id', 'ref'])
+    # Get the best matching samples
+    df_min_error_rates = pd.read_csv(f'{output}/samtools/minimum_error_rates.csv')
+    df_error_rates = df_min_error_rates[['read_id','ref']]
 
-# Get the number of mapped reads for the negative controls of the samples and the rt regions
-reads_neg_ctrl = df_merged.loc[df_merged['read_id'].str.contains('neg_ctrl'), 'mapped_reads'].values[0]
-reads_neg_ctrl_rt = df_merged.loc[df_merged['read_id'].str.contains('neg_ctrl'), 'mapped_reads_rt'].values[0]
+    # Get the number of mapped reads for each sample
+    df_view_stats = pd.read_csv(f'{output}/samtools/view_stats.csv', header=None)
+    df_view_stats.columns = ['read_id', 'ref', 'mapped_reads']
 
-# Calculate the ratio of mapped reads of the negative control to the sample and the rt region
-df_merged['mapped reads neg_ctrl/mapped reads sample'] = round(reads_neg_ctrl / df_merged['mapped_reads'], 3)
-df_merged['mapped reads neg_ctrl_rt/mapped reads sample_rt'] = round(reads_neg_ctrl_rt / df_merged['mapped_reads_rt'], 3)
+    # Get the number of mapped reads in the rt region for each sample
+    df_view_stats_rt = pd.read_csv(f'{output}/samtools/view_stats_rt.csv', header=None)
+    df_view_stats_rt.columns = ['read_id', 'ref', 'mapped_reads_rt']
 
-# Perform QC checks
-df_merged['qc_pass_ctrl'] = (df_merged['mapped reads neg_ctrl/mapped reads sample'] <= 0.1) & (df_merged['mapped_reads'] >= 100)   # Sample should have at least 10 times more reads than neg_ctrl and at least 100 mapped reads
-df_merged['qc_pass_rt'] = (df_merged['mapped reads neg_ctrl_rt/mapped reads sample_rt'] <= 0.1) & (df_merged['mapped_reads_rt'] >= 100)    # Sample should have at least 10 times more reads than neg_ctrl and at least 100 reads mapped to the RT region 
-df_merged['qc_pass'] = df_merged['qc_pass_ctrl'] & df_merged['qc_pass_rt']    # True if sample passes both QC checks
+    # Get the number of mapped reads for the best matching samples
+    df_merged = pd.merge(df_error_rates, df_view_stats, on=['read_id', 'ref'])
+    df_merged = pd.merge(df_merged, df_view_stats_rt, on=['read_id', 'ref'])
 
-# Perform QC check of the negative control
-mapped_reads_tot = df_merged['mapped_reads'].sum()
-reads_neg_ctrl = df_merged.loc[df_merged['read_id'].str.contains('neg_ctrl'), 'mapped_reads'].values[0]
-neg_ctrl_ratio = round(reads_neg_ctrl / mapped_reads_tot, 3)
+    # Get the number of mapped reads for the negative controls of the samples and the rt regions
+    reads_neg_ctrl = df_merged.loc[df_merged['read_id'].str.contains('neg_ctrl'), 'mapped_reads'].values[0]
+    reads_neg_ctrl_rt = df_merged.loc[df_merged['read_id'].str.contains('neg_ctrl'), 'mapped_reads_rt'].values[0]
 
-if neg_ctrl_ratio > 0.1:
-    df_merged.loc[df_merged['read_id'].str.contains('neg_ctrl'), 'qc_pass'] = False
-    print(f'Warning! The negative control contains {neg_ctrl_ratio * 100}% of the reads')
-else:
-    df_merged.loc[df_merged['read_id'].str.contains('neg_ctrl'), 'qc_pass'] = True
+    # Calculate the ratio of mapped reads of the negative control to the sample and the rt region
+    df_merged['mapped reads neg_ctrl/mapped reads sample'] = round(reads_neg_ctrl / df_merged['mapped_reads'], 3)
+    df_merged['mapped reads neg_ctrl_rt/mapped reads sample_rt'] = round(reads_neg_ctrl_rt / df_merged['mapped_reads_rt'], 3)
 
+    return df_merged
 
-## Output the results
-# All results
-df_merged.to_csv(f'{output}/qc.csv', index=False)
+def qc(df_merged):
+    '''Perform QC checks'''
 
-# Results relevant to the clinic
-df_clinic = df_merged[['read_id','ref','mapped_reads','mapped_reads_rt','qc_pass']]
+    # Perform QC checks
+    df_merged['qc_pass_ctrl'] = (df_merged['mapped reads neg_ctrl/mapped reads sample'] <= 0.1) & (df_merged['mapped_reads'] >= 100)   # Sample should have at least 10 times more reads than neg_ctrl and at least 100 mapped reads
+    df_merged['qc_pass_rt'] = (df_merged['mapped reads neg_ctrl_rt/mapped reads sample_rt'] <= 0.1) & (df_merged['mapped_reads_rt'] >= 100)    # Sample should have at least 10 times more reads than neg_ctrl and at least 100 reads mapped to the RT region 
+    df_merged['qc_pass'] = df_merged['qc_pass_ctrl'] & df_merged['qc_pass_rt']    # True if sample passes both QC checks
 
-os.makedirs(f'{output}/clinic', exist_ok=True)
+    # Perform QC check of the negative control
+    mapped_reads_tot = df_merged['mapped_reads'].sum()
+    reads_neg_ctrl = df_merged.loc[df_merged['read_id'].str.contains('neg_ctrl'), 'mapped_reads'].values[0]
+    neg_ctrl_ratio = round(reads_neg_ctrl / mapped_reads_tot, 3)
 
-df_clinic.to_csv(f'{output}/clinic/qc.csv', index=False)
+    if neg_ctrl_ratio > 0.1:
+        df_merged.loc[df_merged['read_id'].str.contains('neg_ctrl'), 'qc_pass'] = False
+        print(f'Warning! The negative control contains {neg_ctrl_ratio * 100}% of the reads')
+    else:
+        df_merged.loc[df_merged['read_id'].str.contains('neg_ctrl'), 'qc_pass'] = True
+
+    return df_merged
+
+def output_results(df_merged, output):
+    '''Output the results'''
+
+    # All results
+    df_merged.to_csv(f'{output}/qc.csv', index=False)
+
+    # Results relevant to the clinic
+    df_clinic = df_merged[['read_id','ref','mapped_reads','mapped_reads_rt','qc_pass']]
+
+    os.makedirs(f'{output}/clinic', exist_ok=True)
+
+    df_clinic.to_csv(f'{output}/clinic/qc.csv', index=False)
+
+def main():
+    output = get_output()
+    df_merged = data_handling()
+    df_merged = qc(df_merged)
+    output_results(df_merged, output)
+
+if __name__ == '__main__':
+    main()
