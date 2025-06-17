46663facbb103bb0cc171ec63febf269e56f3ab9
Daniel Schmidt <daniel.schmidt@gu.se>
2025-06-17 14:57:12 +0200

    Function to get the best matching read_id and reference pairs.

--- workflow/scripts/new_vcf_modification.py
+++ workflow/scripts/new_vcf_modification.py
@@ -2,6 +2,7 @@
 
 import pdb
 
+import csv
 import yaml
 
 
@@ -18,3 +19,14 @@ def get_output():
 breakpoint()
 output = get_output()    # Get the Snakemake output folder
 output = "/clinical/data/hbv_nano/results/250617-094351_250616_hbv_val_03_test"
+
+
+def get_read_id_ref(file_path):
+    '''Get the best matching reference for each read_id'''
+
+    with open(file_path) as csv_file:
+        reader = csv.DictReader(csv_file)
+        return {row['read_id']: row['ref'] for row in reader}
+
+read_id_ref = get_read_id_ref(f'{output}/samtools/minimum_error_rates.csv')    # Read in file containing read_id and its reference
+
