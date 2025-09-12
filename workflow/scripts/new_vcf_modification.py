e5fe276d8e8085dc24315730f5f3ff5197d26a65
Daniel Schmidt <daniel.schmidt@gu.se>
2025-09-12 10:27:20 +0200

    Added function to import reference to split_vcf dictionary.

--- workflow/scripts/new_vcf_modification.py
+++ workflow/scripts/new_vcf_modification.py
@@ -87,22 +87,40 @@ def split_vcf(vcf):
             split_vcf[key] = split_row
     return split_vcf
 
+def add_ref_vcf(split_vcf, ref_seq, ref_genotype):
+    'Add the reference to the split vcf'''
+
+    ref_len = len(ref_seq)
+    for pos in range(1, ref_len + 1):    # For positon in the reference seqeunce (position starts at 1)
+        base = ref_seq[pos - 1]    # Get the base (index starts at 0)
+        if pos in split_vcf:
+            split_vcf[pos][ref_genotype] = base    # If the reference position exists, keep it
+        else:
+            split_vcf[pos] = {'pos': pos, ref_genotype: base}    
+    return split_vcf
 
 def main():
     output = get_snakemake_output()    # Get the Snakemake output folder
     read_id_ref = get_read_id_ref(f'{output}/samtools/minimum_error_rates.csv')    # Read in file containing read_id and its reference
     print(f'{read_id_ref}, read_id_ref' )
-    ref_genomes = get_ref_genomes('reference_genomes')
-    print(f'{ref_genomes}, ref_genomes')
+    x = read_id_ref['KH20-2510']
+    print(f'{x}, x')
+    ref_genomes = get_ref_genomes('reference_genomes')   # get reference genomes from folder reference_genomes
+    #print(f'{ref_genomes}, ref_genomes')    # DEV: Print referenece genomes
+    ref_d = ref_genomes['ref_d']
+    print(ref_d[0:50])
+    #print(f'{ref_genomes['ref_a']}')
+    
     path = f'{output}/freebayes/KH20-2510.ref_d.vcf'
     reader = vcfpy.Reader.from_path(path)
     #vcf = vcf(reader)
     vcf = read_vcf(reader)
     vcf = split_vcf(vcf)
-    '''
-    for pos in range(130, 1161):    # The RT region
+    vcf = add_ref_vcf(vcf, ref_genomes['ref_a'], 'ref_a')
+    #for pos in range(130, 1161):
+    for pos in range(1, 100):    # The RT region
         if pos in vcf:
             print(vcf[pos])
-    '''
+
 if __name__ == '__main__':
     main()
