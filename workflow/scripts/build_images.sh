c0d18678de19e79a9ba6f5cc17665c00bc160012
Daniel Schmidt <daniel.e.schmidt@outlook.com>
2026-06-05 11:23:59 +0200

    Updated fastp to v1.3.3 and pointing to specific versions for all tools

--- workflow/scripts/build_images.sh
+++ workflow/scripts/build_images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
-apptainer build images/fastp_0.23.4.img docker://staphb/fastp
-apptainer build images/freebayes_1.3.7.img docker://staphb/freebayes
-apptainer build images/medaka_1.12.0.img docker://ontresearch/medaka
-apptainer build images/minimap2_2.28.img docker://staphb/minimap2
-apptainer build images/samtools_1.20.img docker://staphb/samtools
+apptainer build images/fastp_1.3.3.img docker://staphb/fastp:1.3.3
+apptainer build images/freebayes_1.3.7.img docker://staphb/freebayes:1.3.7
+apptainer build images/medaka_1.12.0.img docker://ontresearch/medaka:1.12.0
+apptainer build images/minimap2_2.28.img docker://staphb/minimap2:2.28
+apptainer build images/samtools_1.20.img docker://staphb/samtools:1.20
